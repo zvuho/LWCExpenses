4d3151b37644b615e8e7f446545c72fd665a9f1d
ZVUHO <idesalterain@gmail.com>
2022-10-07 17:49:43 -0300

    creator ok, list not ready

--- force-app/main/default/lwc/expenseList/expenseList.js
+++ force-app/main/default/lwc/expenseList/expenseList.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
 
-export default class ExpenseList extends LightningElement {}
+import { LightningElement, wire } from 'lwc';
+import getExpenses from '@salesforce/apex/ExpenseController.getExpenses';
+import { refreshApex } from '@salesforce/apex';
+
+
+const table_columns = [
+    {label:'Label', fieldName: 'Label__c', type: 'text' },
+    {label:'Expense date', fieldName: 'Expense_date__c', type: 'Date' },
+    {label:'Amount', fieldName: 'Amount__c', type: 'Currency' },
+    {label:'Recurrency', fieldName: 'Recurrency__c', type: 'Picklist' },
+
+];
+
+export default class ExpenseList extends LightningElement {
+
+    @wire(getExpenses) expenses;
+    columns = table_columns;
+    
+}
